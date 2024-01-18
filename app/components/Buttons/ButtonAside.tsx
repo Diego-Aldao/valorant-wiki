eb0147ffeb2ceb3a109069a4e59b44bea2ef8635
Diego <diegofrontdev@gmail.com>
2024-01-18 20:23:51 -0300

    añadido el botton para el toggle del aside

---
+++ app/components/Buttons/ButtonAside.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { Icon } from "@iconify/react";
+
+export default function ButtonAside() {
+  return (
+    <div className="mt-auto flex items-center justify-center">
+      <Icon icon="line-md:arrow-open-right" className=" h-6 w-6" />
+    </div>
+  );
+}
