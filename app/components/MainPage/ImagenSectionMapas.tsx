06ce7af83e0124df221107f9eb5422fb8864601b
Diego <diegofrontdev@gmail.com>
2024-01-19 19:41:17 -0300

    añadido el componente que renderiza la imagen de la seccion mapas de la pagina principal

---
+++ app/components/MainPage/ImagenSectionMapas.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import React from "react";
+import mapa from "../../assets/mapa.png";
+export default function ImagenSectionMapas() {
+  return (
+    <div className="hidden md:block w-full h-[372px] lg:h-[472px] after:absolute after:bg-gradient-to-r after:from-main-black after:via-[rgba(0,0,0,0.6)] after:to-transparent after:w-full after:h-full after:top-0 after:left-0  absolute top-16 left-0 -z-[1]">
+      <Image
+        src={mapa}
+        alt="mapa"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
