c5b8ac7c942e5a47cdd66624858220052ce670d1
Diego <diegofrontdev@gmail.com>
2024-01-22 20:21:28 -0300

    creado un custom hook para saber la posicion del scroll Y en el sitio

---
+++ app/hooks/useYScroll.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function useYScroll() {
+  const [scrollY, setScrollY] = useState<number>(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollY;
+}
