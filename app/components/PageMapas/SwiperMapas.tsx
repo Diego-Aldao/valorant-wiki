139e4b1ebdd3f6bf6c598a3b301d97afb74f8621
Diego <diegofrontdev@gmail.com>
2024-01-22 20:20:47 -0300

    creado un componente que renderiza un swiper para la pagina mapas

---
+++ app/components/PageMapas/SwiperMapas.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Swiper } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function SwiperMapas({ children }: Props) {
+  return (
+    <Swiper
+      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      className="relative"
+      loop={true}
+      breakpoints={{ 1240: { slidesPerView: 1.2, spaceBetween: 50 } }}
+    >
+      {children}
+    </Swiper>
+  );
+}
