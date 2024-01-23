07abd5487d1c217fc1ce4490eaf764050d6fc69c
Diego <diegofrontdev@gmail.com>
2024-01-23 20:49:24 -0300

    añadido un componente para renderizar un grid con los agentes hasta los 768px

---
+++ app/components/PageAgentes/GridMobile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { type Agente } from "@/app/types/Agentes";
+import MainButton from "../Buttons/MainButton";
+import CardAgente from "./CardAgente";
+
+interface Props {
+  dataAgentes: Agente[];
+}
+
+export default function GridMobile({ dataAgentes }: Props) {
+  return (
+    <div className="grid-mobile md:hidden grid grid-cols-[repeat(auto-fill,minmax(250px,_1fr))] gap-4 gap-y-16">
+      {dataAgentes.map((agente) => (
+        <React.Fragment key={agente.uuid}>
+          <CardAgente
+            nombreAgente={agente.displayName}
+            imagenAgente={agente.fullPortrait}
+            outsideStyles="h-[450px] max-w-[450px]"
+            insideStyles="w-[200px] h-[400px]"
+          >
+            <MainButton
+              outsideStyles="mx-auto border-main-red min-w-[180px]"
+              insideStyles="text-xs py-1 px-2"
+              nombre={`ver info de ${agente.displayName}`}
+            />
+          </CardAgente>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
