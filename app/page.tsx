9385dd43d39cf235255e99e1fabc3986c755f856
Diego <diegofrontdev@gmail.com>
2024-01-24 20:51:26 -0300

    actualizadas las descripciones y agregados estilos a los mainbuttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Descripciones from "./components/MainPage/Descripciones";
 import ImagenesSectionAgentes from "./components/MainPage/ImagenesSectionAgentes";
-import CustomMain from "./containers/custom-main";
 import SectionAgentes from "./containers/pagina-inicio/section-agentes";
 import HeroSection from "./containers/pagina-inicio/hero-section";
 import SectionMapas from "./containers/pagina-inicio/section-mapas";
@@ -11,18 +10,19 @@ import ImagenSectionArmas from "./components/MainPage/ImagenSectionArmas";
 
 export default function Home() {
   return (
-    <CustomMain>
+    <main className="overflow-x-hidden mt-12 md:mt-16 lg:mt-20">
       <HeroSection />
       <SectionAgentes>
         <ImagenesSectionAgentes />
         <Descripciones
           titulo="agentes"
           subtitulo="la creatividad es tu mejor arma"
-          descripcion="Más que armas y balas, elegirás un agente armado con habilidades adaptativas, rápidas y letales que crean oportunidades para que tu juego de armas brille. No hay dos agentes que jueguen igual, así como no hay dos highlights que se vean iguales."
+          descripcion="Más que armas y balas, podrás elegir un agente armado con habilidades adaptativas, rápidas y letales que crean oportunidades para que tu juego de armas brille. Ningun agente se jugará igual, ni ningun highlight se vera igual."
+          customClases="after:bg-main-white"
           button={
             <MainButton
-              outsideStyles="border-main-white mt-5 ml-auto self-end justify-self-endx"
-              insideStyles="bg-main-white text-main-black text-xs md:text-sm"
+              outsideStyles="border-main-white mt-5 ml-auto after:bg-main-red before:bg-main-red"
+              insideStyles="bg-main-white text-main-black text-xs md:text-sm after:bg-secondary-white"
               nombre="ver todos los agentes"
               link="agentes"
             />
@@ -33,10 +33,11 @@ export default function Home() {
         <Descripciones
           titulo="mapas"
           subtitulo="pelea en todas partes del mundo"
-          descripcion="Cada mapa es un patio de recreo para mostrar tu creatividad. Diseñado específicamente para estrategias de equipo, jugadas espectaculares y momentos decisivos. Haz la jugada que otros imitarán por años."
+          descripcion="Cada mapa es un campo de batalla para mostrar tu creatividad. Diseñados específicamente para estrategias de equipo, jugadas espectaculares y momentos decisivos. Haz la jugada que otros imitarán en el futuro."
+          customClases="after:bg-main-white"
           button={
             <MainButton
-              outsideStyles="border-main-red mt-5 ml-auto justify-self-end"
+              outsideStyles="border-main-red mt-5 ml-auto"
               insideStyles="bg-main-red text-main-white text-xs md:text-sm"
               nombre="ver todos los mapas"
               link="mapas"
@@ -48,20 +49,20 @@ export default function Home() {
       <SectionArmas>
         <ImagenSectionArmas />
         <Descripciones
-          titulo="armas"
-          subtitulo="pelea en todas partes del mundo"
-          descripcion="Cada mapa es un patio de recreo para mostrar tu creatividad. Diseñado específicamente para estrategias de equipo, jugadas espectaculares y momentos decisivos. Haz la jugada que otros imitarán por años."
+          titulo="arsenal"
+          subtitulo="complementa tu estilo de juego"
+          descripcion="Desde armas ligeras y versatiles a maravillas feroces. Encuentra la compañera ideal con la que compartirás tu gloria y llevarás a la ruina al enemigo. Y por supuesto, nunca olvides el cuchillo tactico para soluciones intimas."
           customClases="text-main-black after:bg-main-black"
           button={
             <MainButton
-              outsideStyles="border-main-black mt-5 ml-auto justify-self-end"
-              insideStyles="bg-main-black text-main-white text-xs md:text-sm"
-              nombre="ver todos los mapas"
-              link="mapas"
+              outsideStyles="border-main-black mt-5 ml-auto after:bg-main-white before:bg-main-white"
+              insideStyles="bg-main-black text-main-white text-xs md:text-sm after:bg-main-red"
+              nombre="ver todas las armas"
+              link="armas"
             />
           }
         />
       </SectionArmas>
-    </CustomMain>
+    </main>
   );
 }
