5ceeb4179a16f34a696b13d48049a99d50e351cd
Diego <diegofrontdev@gmail.com>
2024-01-29 20:27:51 -0300

    agregadas las clases que ahora requiere el componente logo. Ahora el componente usa el hook useParamas para saber si esta en una pagina de detalle, y en base a eso cambiar su background

--- app/components/Layout/Header/Header.tsx
+++ app/components/Layout/Header/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useParams } from "next/navigation";
 import ButtonMenuMobile from "../../Buttons/ButtonMenuMobile";
 import Logo from "../../Logo";
 import NavDesktop from "./NavDesktop";
@@ -6,6 +7,7 @@ import useYScroll from "@/app/hooks/useYScroll";
 
 const Header = () => {
   const scrollY = useYScroll();
+  const { id } = useParams();
 
   return (
     <header
@@ -13,8 +15,12 @@ const Header = () => {
         scrollY >= 700 ? "fixed md:left-20 md:w-[calc(100%-80px)]" : "absolute"
       }`}
     >
-      <div className="px-4 md:pr-8 lg:pl-5 lg:pr-10 h-12 md:h-16 lg:h-20 flex justify-between items-center max-w-[1200px] gap-4 bg-main-black mx-auto w-full">
-        <Logo />
+      <div
+        className={`px-4 md:pr-8 lg:pl-6 lg:pr-10 h-12 md:h-16 lg:h-20 flex justify-between items-center max-w-[1200px] gap-4 mx-auto w-full ${
+          !id && "bg-main-black"
+        }`}
+      >
+        <Logo customClasses="h-full w-12 md:hidden" />
         <NavDesktop />
         <ButtonMenuMobile />
         <span className="boton-jugar hidden md:flex items-center justify-center font-bold rounded-lg h-fit px-4 py-2 bg-main-red uppercase text-main-black">
