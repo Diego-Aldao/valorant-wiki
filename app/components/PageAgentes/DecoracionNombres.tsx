b8cb763038baa18cc716a95fdfd3066a1037de66
Diego <diegofrontdev@gmail.com>
2024-01-29 20:33:48 -0300

    agregado un blend mode al contenedor, una key a cada span y actualizada la fuente

--- app/components/PageAgentes/DecoracionNombres.tsx
+++ app/components/PageAgentes/DecoracionNombres.tsx
@@ -11,9 +11,10 @@ export default function DecoracionNombres({ nombre, cantidad }: Props) {
   for (let i = 0; i < cantidad; i++) {
     spansDeNombres.push(
       <span
+        key={i}
         className={`${
-          i >= 1 ? "text-8xl" : "text-9xl"
-        } uppercase relative mix-blend-soft-light font-bebas font-bold`}
+          i >= 1 ? "text-5xl" : "text-6xl"
+        } uppercase relative mix-blend-soft-light font-unbounded leading-[1.4] font-bold`}
       >
         {nombre}
       </span>
@@ -21,7 +22,7 @@ export default function DecoracionNombres({ nombre, cantidad }: Props) {
   }
 
   return (
-    <div className="deco-nombres w-full flex flex-col items-center h-full">
+    <div className="deco-nombres w-full flex flex-col items-center h-full mix-blend-overlay">
       {spansDeNombres}
     </div>
   );
