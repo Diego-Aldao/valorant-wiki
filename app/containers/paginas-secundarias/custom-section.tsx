91b83eee3ac8d39b677a742565df7a27e1ee1ff2
Diego <diegofrontdev@gmail.com>
2024-01-30 19:48:07 -0300

    corregidas las clases y cambiada la fuente del titulo

--- app/containers/paginas-secundarias/custom-section.tsx
+++ app/containers/paginas-secundarias/custom-section.tsx
@@ -8,10 +8,10 @@ interface Props {
 export default function CustomSection({ children, titulo }: Props) {
   return (
     <section className="px-4 md:px-8 lg:px-10 mt-12 md:mt-16 lg:mt-20 ">
-      <header className="py-10">
-        <h1 className="uppercase text-4xl md:text-8xl font-bold md:tracking-tighter md:text-terciary-black text-secondary-black relative lg:text-9xl">
+      <header className="py-10 md:mb-10">
+        <h1 className="uppercase text-3xl md:text-7xl font-bold md:tracking-tighter md:text-terciary-black text-secondary-black relative lg:text-8xl font-unbounded">
           {titulo}
-          <span className="hidden md:block absolute bottom-2 left-1 text-4xl tracking-wide text-secondary-black lg:text-5xl">
+          <span className="hidden md:block absolute bottom-0 left-0 text-4xl tracking-wide text-secondary-black lg:text-5xl">
             {titulo}
           </span>
         </h1>
