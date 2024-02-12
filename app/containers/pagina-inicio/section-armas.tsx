99a9478ccac3395c482c2e0c4738e7622273ce08
Diego <diegofrontdev@gmail.com>
2024-02-12 20:34:44 -0300

    corregidas las decoraciones

--- app/containers/pagina-inicio/section-armas.tsx
+++ app/containers/pagina-inicio/section-armas.tsx
@@ -7,11 +7,10 @@ interface Props {
 
 export default function SectionArmas({ children }: Props) {
   return (
-    <section className="py-10 relative md:py-16 px-4 md:px-8 lg:px-10 flex md:gap-2 lg:gap-6 items-center bg-main-white mt-20">
-      <div className="border-top-right-1 absolute -right-10 -top-[39px] bg-main-white w-[60%] skew-x-[-45deg] rounded-t-sm h-10 z-20"></div>
-      <div className="border-top-left-1 absolute -left-10 -top-20 bg-main-white w-[120px] skew-x-[45deg] rounded-t-sm h-20 z-20 hidden md:block"></div>
-      <div className="border-top-left-1 absolute left-52 top-0 bg-main-black w-[220px] skew-x-[45deg] rounded-t-sm h-5 z-20 hidden xl:block after:absolute after:-right-6 after:top-0 after:w-[100px] after:h-full after:bg-main-black after:skew-x-[-65deg]"></div>
-      <div className="border-top-right-2 absolute -right-10 -top-20 bg-main-white w-[400px] skew-x-[-45deg] rounded-t-sm h-20 z-20 hidden lg:block"></div>
+    <section className="py-10 pt-20 relative md:py-16 md:pt-[120px] md:mt-0 px-4 md:px-8 lg:px-10 flex md:gap-2 lg:gap-6 items-center bg-main-white overflow-hidden">
+      <div className="border-top-right-1 absolute left-20 top-0 bg-main-black w-[100%] skew-x-[45deg]  h-10 md:h-5 z-20"></div>
+      <div className="border-top-right-2 absolute left-[110px] top-5 bg-main-black w-[55%] skew-x-[45deg]  h-10 z-20 hidden md:block after:absolute after:bg-main-black after:-right-12 after:top-0 after:w-[200px] after:h-full after:skew-x-[-65deg]"></div>
+      <div className="border-top-left-1 absolute left-52 top-[60px] bg-main-black w-[220px] skew-x-[45deg] rounded-t-sm h-5 z-20 hidden lg:block after:absolute after:-right-6 after:top-0 after:w-[100px] after:h-full after:bg-main-black after:skew-x-[-65deg]"></div>
       {children}
     </section>
   );
