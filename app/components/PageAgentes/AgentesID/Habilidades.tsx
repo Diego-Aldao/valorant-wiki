16333b12c46a27a3d09801fb84043d33da3b59a3
Diego <diegofrontdev@gmail.com>
2024-02-13 16:15:55 -0300

    añadido un componente que muestra las habilidades del agente

---
+++ app/components/PageAgentes/AgentesID/Habilidades.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { Ability } from "../../../types/Agentes";
+import React, { useState } from "react";
+import { listadoSlots } from "@/app/constants";
+
+interface Props {
+  habilidades: Ability[];
+}
+
+export default function Habilidades({ habilidades }: Props) {
+  const [currentHabilidad, setCurrentHabilidad] = useState<Ability>(
+    habilidades[0]
+  );
+
+  return (
+    <div className="habilidades flex flex-col gap-5">
+      <div className="iconos flex gap-4 justify-center md:justify-start mt-5">
+        {habilidades.map((habilidad) => (
+          <React.Fragment key={habilidad.slot}>
+            {habilidad.displayIcon && (
+              <span
+                onClick={() => {
+                  setCurrentHabilidad(habilidad);
+                }}
+                className={`p-2 cursor-pointer border-secondary-black border-[1px] rounded-sm transition-colors ${
+                  currentHabilidad === habilidad
+                    ? "md:bg-main-black bg-main-white"
+                    : "bg-transparent"
+                }`}
+              >
+                <Image
+                  src={habilidad.displayIcon || ""}
+                  alt={habilidad.displayName}
+                  height={35}
+                  width={35}
+                  className={`transition-opacity ${
+                    currentHabilidad === habilidad
+                      ? "opacity-100 invert md:invert-0"
+                      : "md:invert opacity-40"
+                  }`}
+                />
+              </span>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="desc-habilidad flex flex-col gap-2">
+        <span className="uppercase font-bold text-lg text-center md:text-left md:text-main-black">
+          {listadoSlots[currentHabilidad.slot]} - {currentHabilidad.displayName}
+        </span>
+        <p className="leading-6 text-sm w-full text-center md:text-left md:text-main-black font-medium">
+          {currentHabilidad?.description}
+        </p>
+      </div>
+    </div>
+  );
+}
