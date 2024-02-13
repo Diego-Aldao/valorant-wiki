6ae2b5c39728c66d5ee61c4ee3327200714f58ce
Diego <diegofrontdev@gmail.com>
2024-02-13 16:13:50 -0300

    ahora el boton recibe por props un estado para abrir y cerrar el modal

--- app/components/Buttons/ButtonAside.tsx
+++ app/components/Buttons/ButtonAside.tsx
@@ -2,10 +2,25 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 
-export default function ButtonAside() {
+interface Props {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function ButtonAside({ isOpen, setIsOpen }: Props) {
   return (
-    <div className="mt-auto flex items-center justify-center">
-      <Icon icon="line-md:arrow-open-right" className=" h-6 w-6" />
+    <div
+      className="mt-auto flex items-center justify-center"
+      onClick={() => {
+        setIsOpen(!isOpen);
+      }}
+    >
+      <Icon
+        icon="line-md:arrow-open-right"
+        className={`transition-transform h-6 w-6 ${
+          isOpen ? "rotate-[180deg]" : "rotate-0"
+        }`}
+      />
     </div>
   );
 }
