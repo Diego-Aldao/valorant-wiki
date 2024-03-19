546939ed2abc0894e147e8e3a3cf315384375d2a
Diego Aldao <diegofrontdev@gmail.com>
2024-03-19 17:08:04 -0300

    actualizadas las rutas del content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors:{
-        "main-red":"#fe4655",
-        "secondary-red":"#ff7864",
-        "main-black":"#302732",
-        "secondary-black":"#8c818a",
-        "terciary-black":"#3c323e",
-        "main-white":"#ece8e1",
-        "secondary-white":"#b9b0b8",
+      colors: {
+        "main-red": "#fe4655",
+        "secondary-red": "#ff7864",
+        "main-black": "#302732",
+        "secondary-black": "#8c818a",
+        "terciary-black": "#3c323e",
+        "main-white": "#ece8e1",
+        "secondary-white": "#b9b0b8",
       },
-      fontFamily:{
+      fontFamily: {
         sans: "var(--raleway-font)",
-        unbounded: "var(--unbounded-font)"
+        unbounded: "var(--unbounded-font)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
