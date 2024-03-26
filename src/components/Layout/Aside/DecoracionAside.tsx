370b9362102d93b2dd434b686b20c635bdae53d7
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:04:22 -0300

    ahora el componente recibe clases custom por props. Modificado su color

--- src/components/Layout/Aside/DecoracionAside.tsx
+++ src/components/Layout/Aside/DecoracionAside.tsx
@@ -1,19 +1,20 @@
 interface Props {
   isRotated?: boolean;
+  customClases?: string;
 }
 
-export default function DecoracionAside({ isRotated }: Props) {
+export default function DecoracionAside({ isRotated, customClases }: Props) {
   return (
     <div
       className={`alt hidden lg:flex gap-5 justify-center items-center ${
         isRotated ? "flex-col-reverse" : "flex-col"
-      }`}
+      } ${customClases}`}
     >
-      <span className="uppercase text-xs [writing-mode:vertical-lr] text-secondary-black font-semibold tracking-wide">
+      <span className="uppercase text-xs [writing-mode:vertical-lr] text-main-red font-semibold tracking-wide">
         valorant wiki
       </span>
       <div
-        className={`deco hidden lg:flex h-20 w-[2px] bg-secondary-black items-center justify-center relative after:absolute after:rotate-45 after:bg-secondary-black after:w-2 after:h-2 ${
+        className={`deco hidden lg:flex h-20 w-[2px] bg-main-red items-center justify-center relative after:absolute after:rotate-45 after:bg-main-red after:w-2 after:h-2 ${
           isRotated ? "after:top-0" : "after:bottom-0"
         }`}
       ></div>
