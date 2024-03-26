4669383855eeb6302c1f6b29546e407833d266f6
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:07:33 -0300

    añadidos condicionales para cambiar el background de cada link dependiendo la pagina. Modificadas clases tailwind

--- src/components/Layout/Header/NavDesktop.tsx
+++ src/components/Layout/Header/NavDesktop.tsx
@@ -12,14 +12,19 @@ const NavDesktop = () => {
   if (pathname === "/") miPathname = "/inicio";
 
   return (
-    <div className="hidden md:flex items-center gap-2">
-      <ul className="flex font-jost gap-2 lg:gap-4">
+    <div className="hidden md:flex items-center gap-2 justify-center pointer-events-auto">
+      <ul className="flex font-jost gap-2 lg:gap-8 rounded-xl h-full">
         {listadoPages.map((page) => (
-          <li key={page.id} className="px-4 py-2 ">
+          <li
+            key={page.id}
+            className={`px-4 py-2 flex items-center justify-center [backdrop-filter:blur(15px)] rounded-md ${
+              miPathname.includes(page.nombre) && "bg-main-red"
+            } ${miPathname.includes(page.nombre) && id && "!bg-main-black"} `}
+          >
             <Link
               className={`${id && "md:text-main-black"} ${
-                miPathname.includes(page.nombre) && "md:!text-main-red"
-              } text-secondary-white capitalize tracking-wide text-sm lg:text-base  font-semibold`}
+                miPathname.includes(page.nombre) && "md:text-main-white"
+              } capitalize tracking-wide text-sm lg:text-base  font-semibold`}
               href={`/${page.nombre !== "inicio" ? page.nombre : ""}`}
             >
               {page.nombre}
