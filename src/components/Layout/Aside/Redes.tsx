dc286d3e1b2949d297134e9f3014594ef2d6d5b5
Diego Aldao <diegofrontdev@gmail.com>
2024-03-26 17:34:24 -0300

    añadido un link para ir a la pagina de la red social

--- src/components/Layout/Aside/Redes.tsx
+++ src/components/Layout/Aside/Redes.tsx
@@ -13,19 +13,22 @@ export default function Redes({ listStyles, itemStyles, textStyles }: Props) {
   return (
     <ul className={`flex flex-col w-full ${listStyles}`}>
       {listadoRedes.map(({ icono, nombre, id }) => (
-        <li
-          className={`flex gap-4 w-40 py-2 group cursor-pointer ${itemStyles}`}
-          key={id}
-        >
-          <Icon
-            icon={`line-md:${icono}`}
-            className="w-5 h-5 group-hover:text-main-red transition-colors"
-          />
-          <span
-            className={`text-sm font-medium group-hover:text-main-red transition-colors ${textStyles}`}
+        <li key={id}>
+          <a
+            href={`https://${nombre}.com`}
+            target="_blank"
+            className={`flex gap-4 w-40 py-2 group  ${itemStyles}`}
           >
-            {nombre}
-          </span>
+            <Icon
+              icon={`line-md:${icono}`}
+              className="w-5 h-5 group-hover:text-main-red transition-colors"
+            />
+            <span
+              className={`text-sm font-medium group-hover:text-main-red transition-colors ${textStyles}`}
+            >
+              {nombre}
+            </span>
+          </a>
         </li>
       ))}
     </ul>
