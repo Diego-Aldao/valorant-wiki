4fc5bf6d45af58edc3fe428058d05a2b5c603628
Diego Aldao <diegofrontdev@gmail.com>
2024-03-26 17:36:55 -0300

    añadidas las listas de links en los botones agentes y armas

--- src/components/Layout/Header/NavDesktop.tsx
+++ src/components/Layout/Header/NavDesktop.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import React from "react";
-import { listadoPages } from "@/src/constants";
+import { listadoPages, listadoAgentes, listadoArmas } from "@/src/constants";
+import ListaMenuMobile from "../MenuMobile/ListaMenuMobile";
 
 const NavDesktop = () => {
   const { id } = useParams();
@@ -14,23 +15,46 @@ const NavDesktop = () => {
   return (
     <div className="hidden md:flex items-center gap-2 justify-center pointer-events-auto">
       <ul className="flex font-jost gap-2 lg:gap-8 rounded-xl h-full">
-        {listadoPages.map((page) => (
-          <li
-            key={page.id}
-            className={`px-4 py-2 flex items-center justify-center [backdrop-filter:blur(15px)] rounded-md ${
-              miPathname.includes(page.nombre) && "bg-main-red"
-            } ${miPathname.includes(page.nombre) && id && "!bg-main-black"} `}
-          >
-            <Link
-              className={`${id && "md:text-main-black"} ${
-                miPathname.includes(page.nombre) && "md:text-main-white"
-              } capitalize tracking-wide text-sm lg:text-base  font-semibold`}
-              href={`/${page.nombre !== "inicio" ? page.nombre : ""}`}
+        {listadoPages.map((page) =>
+          page.nombre === "agentes" || page.nombre === "armas" ? (
+            <React.Fragment key={page.id}>
+              <ListaMenuMobile
+                nombreLista={page.nombre}
+                lista={
+                  page.nombre === "agentes" ? listadoAgentes : listadoArmas
+                }
+                customLiClasses={`px-4 flex flex-col items-center justify-center [backdrop-filter:blur(15px)] rounded-sm cursor-pointer ${
+                  miPathname.includes(page.nombre) && "bg-main-red"
+                } ${
+                  miPathname.includes(page.nombre) && id && "!bg-main-black"
+                }`}
+                customSpanClasses={`tracking-wide text-sm lg:text-base font-semibold gap-2 ${
+                  id && "md:text-main-black"
+                } ${miPathname.includes(page.nombre) && "md:text-main-white"}`}
+                customIconClasses={`${id && "md:text-main-red"} ${
+                  miPathname.includes(page.nombre) && "md:text-main-white"
+                }`}
+                customUlClasses="absolute top-7 left-0 w-[150px] lg:w-[200px] rounded-sm overflow-hidden"
+              />
+            </React.Fragment>
+          ) : (
+            <li
+              key={page.id}
+              className={`px-4 py-2 flex items-center justify-center [backdrop-filter:blur(15px)] rounded-md ${
+                miPathname.includes(page.nombre) && "bg-main-red"
+              } ${miPathname.includes(page.nombre) && id && "!bg-main-black"}`}
             >
-              {page.nombre}
-            </Link>
-          </li>
-        ))}
+              <Link
+                className={`capitalize tracking-wide text-sm lg:text-base font-semibold ${
+                  id && "md:text-main-black"
+                } ${miPathname.includes(page.nombre) && "md:text-main-white"}`}
+                href={`/${page.nombre !== "inicio" ? page.nombre : ""}`}
+              >
+                {page.nombre}
+              </Link>
+            </li>
+          )
+        )}
       </ul>
     </div>
   );
