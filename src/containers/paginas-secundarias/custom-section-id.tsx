fc21567da25bf98f6a68d54d75ab81f166c282db
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:24:15 -0300

    añadido un contenedor con un max width para no estirar tanto el contenido. cambiado el after de izquierda a derecha. Añadido un after desde el 50% de la section para matchear el color del after del contenedor

--- src/containers/paginas-secundarias/custom-section-id.tsx
+++ src/containers/paginas-secundarias/custom-section-id.tsx
@@ -13,15 +13,17 @@ export default function CustomSectionID({
   customClassesSection,
 }: Props) {
   return (
-    <section
-      className={`px-4 md:px-8 h-full lg:px-10 mt-96 md:mt-0 md:pt-20 lg:pt-[120px] py-10 flex flex-col gap-10  overflow-hidden lg:min-h-[820px] md:items-start after:absolute after:-left-44 after:top-0 after:w-[95%] lg:after:w-[80%] lg:after:-left-48 after:skew-x-[-25deg] after:h-full after:bg-main-white after:-z-[2] after:hidden after:md:block relative min-h-[calc(100vh-88px)] ${customClassesSection}`}
-    >
-      <header className={customClassesTitulo}>
-        <h1 className="uppercase text-4xl sm:text-5xl break-words md:text-5xl font-bold text-main-white md:text-main-black md:text-left text-center relative lg:text-7xl xl:text-[84px] leading-[1] font-unbounded md:[text-shadow:_-1px_-1px_0_#ece8e1,_1px_-1px_0_#ece8e1,_-1px_1px_0_#ece8e1,_1px_1px_0_#ece8e1]">
-          {titulo}
-        </h1>
-      </header>
-      {children}
+    <section className="w-full xl:after:absolute relative after:w-[50%] after:h-full after:right-0 after:top-0 after:bg-main-black bg-main-black xl:bg-main-white">
+      <div
+        className={`px-4 z-[2] md:px-8 min-h-[100vh] lg:h-[calc(100vh-24px)] lg:px-10 pt-32 lg:pt-[120px] py-10 flex flex-col gap-10  overflow-hidden lg:min-h-[820px] md:items-start md:after:absolute after:w-[65%] after:-right-64 after:bg-main-black after:skew-x-[-25deg] after:h-full xl:after:w-[55%] after:top-0 md:bg-main-white relative ${customClassesSection}`}
+      >
+        <header className={customClassesTitulo}>
+          <h1 className="uppercase text-4xl sm:text-5xl break-words md:text-5xl font-bold text-main-white md:text-main-black md:text-left text-center relative lg:text-7xl xl:text-[84px] leading-[1] font-unbounded md:[text-shadow:_-1px_-1px_0_#ece8e1,_1px_-1px_0_#ece8e1,_-1px_1px_0_#ece8e1,_1px_1px_0_#ece8e1]">
+            {titulo}
+          </h1>
+        </header>
+        {children}
+      </div>
     </section>
   );
 }
