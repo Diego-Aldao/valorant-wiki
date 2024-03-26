d92be9c0e0a27ab71a27e8d6c6fb156724e0842b
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:06:24 -0300

    quitado el hook useParams, agregado el componente MenuMobile y un estado para abrir o cerrarlo. Modificadas clases tailwind

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,33 +1,41 @@
 "use client";
-import { useParams } from "next/navigation";
 import ButtonMenuMobile from "../../Buttons/ButtonMenuMobile";
 import Logo from "../../Logo";
 import NavDesktop from "./NavDesktop";
 import useYScroll from "@/src/hooks/useYScroll";
+import MenuMobile from "../MenuMobile/MenuMobile";
+import { useState } from "react";
 
 const Header = () => {
   const scrollY = useYScroll();
-  const { id } = useParams();
+  const [menuMobileVisible, setMenuMobileVisible] = useState<boolean>(true);
 
   return (
-    <header
-      className={`top-0 z-30 w-full left-0 ${
-        scrollY >= 700 ? "fixed md:left-20 md:w-[calc(100%-80px)]" : "absolute"
-      }`}
-    >
-      <div
-        className={`px-4 md:pr-8 lg:pl-6 lg:pr-10 h-12 md:h-16 lg:h-20 flex justify-between items-center max-w-[1200px] gap-4 mx-auto w-full ${
-          !id && "bg-main-black"
-        }`}
+    <>
+      <header
+        className={`top-0 pointer-events-none z-30 w-full px-4 md:px-8 lg:px-10 pt-4 transition-opacity ${
+          scrollY >= 200 && "opacity-0"
+        } ${
+          scrollY >= 700
+            ? "fixed bg-gradient-to-b from-[#302732] to-transparent h-48 opacity-100"
+            : "absolute"
+        } `}
       >
-        <Logo customClasses="h-full w-12 md:hidden" />
-        <NavDesktop />
-        <ButtonMenuMobile />
-        <span className="boton-jugar hidden md:flex items-center justify-center font-bold rounded-lg h-fit px-4 py-2 bg-main-red uppercase text-main-black">
-          juega ahora
-        </span>
-      </div>
-    </header>
+        <div className="flex justify-between items-center gap-4 md:gap-8 lg:gap-12 w-full">
+          <Logo customClasses="[backdrop-filter:blur(15px)] rounded-xl px-1" />
+          <ButtonMenuMobile setMenuMobileVisible={setMenuMobileVisible} />
+          <NavDesktop />
+
+          <span className="boton-jugar ml-auto w-fit hidden md:flex items-center justify-center font-bold rounded-lg h-fit px-4 py-2 bg-main-red uppercase text-main-white">
+            juega ahora
+          </span>
+        </div>
+      </header>
+      <MenuMobile
+        menuMobileVisible={menuMobileVisible}
+        setMenuMobileVisible={setMenuMobileVisible}
+      />
+    </>
   );
 };
 
