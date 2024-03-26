83afa617b1838fb1559305c6c2933c8d0fd19964
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:25:22 -0300

    ahora el componente recibe la prop pageAgentes para modificar los estilos dependiendo de si se va a usar en la pagina Agentes o no

--- src/containers/paginas-secundarias/custom-section.tsx
+++ src/containers/paginas-secundarias/custom-section.tsx
@@ -3,12 +3,21 @@ import React from "react";
 interface Props {
   children: React.ReactNode;
   titulo: string;
+  pageAgentes?: boolean;
 }
 
-export default function CustomSection({ children, titulo }: Props) {
+export default function CustomSection({
+  children,
+  titulo,
+  pageAgentes,
+}: Props) {
   return (
-    <section className="px-4 md:px-8 lg:px-10 mt-12 md:mt-16 lg:mt-20 ">
-      <header className="py-10 md:mb-10">
+    <section
+      className={`px-4 mt-12 md:mt-16 lg:mt-20 ${
+        pageAgentes ? "px-0" : "md:px-8 lg:px-10"
+      }`}
+    >
+      <header className={`py-10 md:mb-10 ${pageAgentes && "md:px-8 lg:px-10"}`}>
         <h1 className="uppercase text-3xl md:text-7xl font-bold md:tracking-tighter md:text-terciary-black text-secondary-black relative lg:text-8xl font-unbounded">
           {titulo}
           <span className="hidden md:block absolute bottom-0 left-0 text-4xl tracking-wide text-secondary-black lg:text-5xl">
