e4cc54f564631f8c6b04ea69129397e8eb33fe48
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:02:21 -0300

    ahora el componente recibe por props la funcion para cambiar el estado menuMobileVisible

--- src/components/Buttons/ButtonMenuMobile.tsx
+++ src/components/Buttons/ButtonMenuMobile.tsx
@@ -1,9 +1,19 @@
 "use client";
 import { Icon } from "@iconify/react";
+import { Dispatch, SetStateAction } from "react";
 
-const ButtonMenuMobile = () => {
+interface Props {
+  setMenuMobileVisible: Dispatch<SetStateAction<boolean>>;
+}
+
+const ButtonMenuMobile = ({ setMenuMobileVisible }: Props) => {
   return (
-    <div className="boton-menu-mobile h-full w-12 md:hidden flex items-center justify-center">
+    <div
+      className="boton-menu-mobile md:hidden pointer-events-auto flex items-center justify-center "
+      onClick={() => {
+        setMenuMobileVisible((prevState) => !prevState);
+      }}
+    >
       <Icon icon="jam:menu" className="h-12 w-12" />
     </div>
   );
