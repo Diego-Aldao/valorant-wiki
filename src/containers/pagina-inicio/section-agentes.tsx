96168b33b637c5506db65831c7f7dfce1909a714
Diego Aldao <diegofrontdev@gmail.com>
2024-03-25 22:20:48 -0300

    modificadas clases tailwind y añadido un contenedor con un width maximo para no estirar tanto el contenido

--- src/containers/pagina-inicio/section-agentes.tsx
+++ src/containers/pagina-inicio/section-agentes.tsx
@@ -6,15 +6,20 @@ interface Props {
 
 const SectionAgentes = ({ children }: Props) => {
   return (
-    <div className="py-10 mb-10 md:py-16 lg:mb-20 px-4 md:px-8 lg:px-10 flex md:gap-2 lg:gap-6 items-center bg-gradient-to-t from-main-red to-secondary-red relative w-full">
+    <div className="pt-10 pb-20 mb-10 md:pb-16 md:pt-16 px-4 md:px-8 lg:px-10 2xl:px-0 bg-gradient-to-t from-main-red to-secondary-red relative w-full">
+      <div
+        id="agentes"
+        className="to-scroll absolute -top-40 lg:-top-60 -z-20"
+      ></div>
       <div className="border-top-left-2 absolute -left-10 -top-10 bg-secondary-red w-[50%] skew-x-[45deg] rounded-t-sm h-10"></div>
       <div className="border-top-left-1 absolute -left-8 -top-[80px] bg-secondary-red w-[50px] skew-x-[45deg] rounded-t-sm h-10 hidden md:block"></div>
       <div className="border-top-right-1 absolute -right-10 -top-20 bg-secondary-red w-[200px] skew-x-[-45deg] rounded-t-sm h-20 hidden lg:block"></div>
-      {children}
-
-      <div className="border-bottom-right-1 absolute -right-10 -bottom-10 bg-main-red w-[70%] skew-x-[45deg] rounded-t-sm h-10"></div>
-      <div className="border-bottom-right-2 absolute -right-20 -bottom-20 bg-main-red w-[70%] skew-x-[45deg] rounded-t-sm h-10 hidden lg:block"></div>
-      <div className="border-bottom-right-2.1 absolute -right-10 -bottom-20 bg-main-black w-[200px] skew-x-[-45deg] rounded-t-sm h-10 hidden lg:block"></div>
+      <div className="contenido-agentes w-full max-w-7xl mx-auto flex md:gap-4 lg:gap-0 lg:justify-between items-center">
+        {children}
+      </div>
+      <div className="border-bottom-left-1 absolute -left-14 -bottom-0 bg-main-black w-[200px] skew-x-[45deg] rounded-t-sm h-10"></div>
+      <div className="border-bottom-right-2 absolute -right-20 -bottom-10 bg-main-red w-[70%] skew-x-[45deg] rounded-t-sm h-10 hidden lg:block"></div>
+      <div className="border-bottom-right-2.1 absolute -right-10 -bottom-10 bg-main-black w-[200px] skew-x-[-45deg] rounded-t-sm h-10 hidden lg:block"></div>
     </div>
   );
 };
--- src/containers/pagina-inicio/section-mapas.tsx
+++ src/containers/pagina-inicio/section-mapas.tsx
@@ -1,5 +1,3 @@
-import MainButton from "@/src/components/Buttons/MainButton";
-import Descripciones from "@/src/components/MainPage/Descripciones";
 import React from "react";
 
 interface Props {
@@ -8,8 +6,10 @@ interface Props {
 
 export default function SectionMapas({ children }: Props) {
   return (
-    <section className="relative py-10 md:py-16 px-4 md:px-8 lg:px-10 flex md:gap-2 lg:gap-6 items-center md:h-[500px] lg:h-[600px]">
-      {children}
+    <section className="py-10 md:py-16 px-4 md:px-8 lg:px-10 md:h-[500px] lg:h-[600px]">
+      <div className="relative contenido-agentes w-full h-full max-w-7xl mx-auto flex md:gap-2 lg:gap-6 items-center">
+        {children}
+      </div>
     </section>
   );
 }
